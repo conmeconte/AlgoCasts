15fdeb4d16739186a101e3b84690a999658ab162
John Hong <john@arcules.com>
2019-12-30 10:39:14 -0800

    levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counterArr = [0];
+  const tempArr = [root, 's'];
+
+  while(tempArr.length){
+    if(tempArr.length === 1 && tempArr[0] === 's') return counterArr;
+    if(tempArr[0] !== 's'){
+      counterArr[counterArr.length-1]+=1;
+      const node = tempArr.shift();
+      tempArr.push(...node.children)
+    }else{
+      counterArr.push(0);
+      const block = tempArr.shift();
+      tempArr.push(block);
+    }
+  };
+  // return counterArr
+}
 
 module.exports = levelWidth;
